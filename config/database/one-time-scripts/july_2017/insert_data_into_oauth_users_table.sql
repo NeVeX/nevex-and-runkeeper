48f3f9524cc47d8a2d8e5bd02571897a90eab8c3
Mark Cunningham <mcunningham@prosper.com>
2017-07-06 15:49:53 -0700

    Change order of insert columns for database example script

--- config/database/one-time-scripts/july_2017/insert_data_into_oauth_users_table.sql
+++ config/database/one-time-scripts/july_2017/insert_data_into_oauth_users_table.sql
@@ -1,4 +1,4 @@
-insert into nevex.oauth_users(user_id, code, created_date, access_token, is_active)
+insert into nevex.oauth_users(user_id, created_date, access_token, is_active, code)
 values
-(200, 'I AM A CODE', '2017-06-01 18:00:00 +00:00', 'I AM ACCESS TOKEN', true),
-(201, 'I AM A CODE 2', '2017-07-02 20:30:00 +00:00', 'I AM ACCESS TOKEN 2', true);
+(200, '2017-06-01 18:00:00 +00:00', 'I AM ACCESS TOKEN', true, 'I AM A CODE'),
+(201, '2017-07-02 20:30:00 +00:00', 'I AM ACCESS TOKEN 2', true, 'I AM A CODE 2');
